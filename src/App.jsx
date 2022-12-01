70c3fb96e2c3687aaa2fd672b0a69c46cd42989a
raibove <shweta.kale@thoughtworks.com>
2022-12-01 07:04:46 +0530

    add circle in d3

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import D3Pie from './components/d3Pie/D3Pie'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <D3Pie />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/d3Pie/D3Pie.jsx
@@ -0,0 +1,54 @@
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
+const D3Pie = () => {
+    const dataFetchedRef = useRef(false);
+  let arrAngle = [];
+  let angleIndex = 0;
+  let pi = Math.PI;
+
+  let newAngle = 0;
+  let tempAngle = 0;
+  let height = 300,
+    width = 300,
+    margin = { top: 15, left: 15, bottom: 15, right: 15 };
+  let radius = (height - margin.top - margin.bottom) / 2;
+
+  let angularScale = d3.scaleLinear().range([0, 359]);
+  let abval = angularScale.invert(0);
+
+  const createSvg = () => {
+    const parent = d3
+      .select(".ring-input")
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const ring = parent
+      .append("g")
+      .attr("id", "rim")
+      .attr("transform", "translate(" + radius + "," + radius + ")");
+
+    ring
+      .append("circle")
+      .attr("r", radius)
+      .attr("class", "ring")
+      .attr("id", "ring")
+      .attr("fill", "white");
+  };
+
+  useEffect(() => {
+       if (dataFetchedRef.current) return;
+      dataFetchedRef.current = true;
+    createSvg();
+  }, []);
+
+  return (
+    <div>
+      <div className="ring-input"></div>
+    </div>
+  );
+};
+
+export default D3Pie;
