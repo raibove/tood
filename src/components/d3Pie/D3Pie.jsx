fff9eff93c37bd6f86fea721d710c09dccf87621
Shweta Kale <shwetakale144@gmail.com>
2022-12-01 07:17:52 +0530

    added drag to handle

--- src/components/d3Pie/D3Pie.jsx
+++ src/components/d3Pie/D3Pie.jsx
@@ -2,7 +2,6 @@ import * as d3 from "d3";
 import { useEffect, useRef } from "react";
 const D3Pie = () => {
   const dataFetchedRef = useRef(false);
-  let arrAngle = [];
   let angleIndex = 0;
   let pi = Math.PI;
 
@@ -37,6 +36,17 @@ const D3Pie = () => {
       .attr("id", "ring")
       .attr("fill", "white");
 
+    const drag = d3
+      .drag()
+      .subject(function (event, d) {
+        return event;
+      })
+      .on("drag", dragmove)
+      .on("end", function (d) {
+        newAngle = tempAngle;
+        angleIndex++;
+      });
+
     const hndl = ring
       .append("circle")
       .attr("r", 10)
@@ -46,7 +56,41 @@ const D3Pie = () => {
         return (
           "rotate(" + angularScale(abval) + ")  translate(0,-" + radius + ")"
         );
-      });
+      })
+      .call(drag);
+
+    function dragmove(d, i) {
+      if (tempAngle != 360) {
+        let coordinates = d3.pointer(d.sourceEvent, parent.node());
+        let x = coordinates[0] - radius;
+        let y = coordinates[1] - radius;
+        let currentAngle = Math.atan2(y, x) * (180 / pi);
+
+        if (currentAngle < -90) {
+          currentAngle += 360 + 90;
+        } else {
+          currentAngle += 90;
+        }
+
+        if (currentAngle > 359) {
+          currentAngle = 360;
+        }
+        if (tempAngle == 0 && currentAngle > 290) {
+          return;
+        }
+        if (currentAngle > newAngle) {
+          tempAngle = currentAngle;
+
+          abval = angularScale.invert(currentAngle);
+        }
+
+        d3.select(this).attr("transform", function (d) {
+          return (
+            "rotate(" + angularScale(abval) + ")  translate(0,-" + radius + ")"
+          );
+        });
+      }
+    }
   };
 
   useEffect(() => {
