30a48f1de35cb7686c8c13b63bd184d97fe502fc
raibove <shwetakale144@gmail.com>
2022-12-08 17:07:00 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import "./App.css";
 import D3Pie from "./components/d3Pie/D3Pie";
 import Landing from "./components/landing/Landing";
 import Register from "./components/register/Register";
+import {
+  BrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <D3Pie /> */}
-      {/* <Landing /> */}
-      {/* <Register title="Register" /> */}
-      <Register title="Login" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Register title="Login" />} />
+          <Route path="/register" element={<Register title="Register" />} />
+          <Route path="/to-do" element={<D3Pie />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
